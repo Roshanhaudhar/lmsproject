0e3a8acc3678cbb4aba5a0c341a1c539a4233075
Roshanhaudhar <chaudharyrosan009@gmail.com>
2024-02-27 10:10:50 +0545

    fix:quill-error-feedback-model

--- components/editor.tsx
+++ components/editor.tsx
@@ -3,25 +3,25 @@
 import dynamic from "next/dynamic";
 import { useMemo } from "react";
 
-import "react-quill/dist/quill.snow.css";
+// import "react-quill/dist/quill.snow.css";
 
 interface EditorProps {
   onChange: (value: string) => void;
   value: string;
-};
+}
 
-export const Editor = ({
-  onChange,
-  value,
-}: EditorProps) => {
-  const ReactQuill = useMemo(() => dynamic(() => import("react-quill"), { ssr: false }), []);
+export const Editor = ({ onChange, value }: EditorProps) => {
+  // const ReactQuill = useMemo(
+  //   () => dynamic(() => import("react-quill"), { ssr: false }),
+  //   []
+  // );
 
   return (
     <div className="bg-white">
-      <ReactQuill
-        theme="snow"
-        value={value}
-        onChange={onChange}
+      <textarea
+        name="change"
+        onChange={(e) => onChange(e.target.value)}
+        id="change"
       />
     </div>
   );
--- components/preview.tsx
+++ components/preview.tsx
@@ -3,22 +3,14 @@
 import dynamic from "next/dynamic";
 import { useMemo } from "react";
 
-import "react-quill/dist/quill.bubble.css";
+// import "react-quill/dist/q÷uill.bubble.css";
 
 interface PreviewProps {
   value: string;
-};
+}
 
-export const Preview = ({
-  value,
-}: PreviewProps) => {
-  const ReactQuill = useMemo(() => dynamic(() => import("react-quill"), { ssr: false }), []);
+export const Preview = ({ value }: PreviewProps) => {
+  // const ReactQuill = useMemo(() => dynamic(() => import("react-quill"), { ssr: false }), []);
 
-  return (
-    <ReactQuill
-      theme="bubble"
-      value={value}
-      readOnly
-    />
-  );
+  return <div>{value}</div>;
 };
